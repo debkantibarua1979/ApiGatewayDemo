6eb19417fccfd3f50268c0ebde2f96787b58f9d0
debkantibarua1979 <129514042+debkantibarua1979@users.noreply.github.com>
2025-06-26 15:14:24 +0600

    Add RestrictAccessMiddleware to enforce referrer validation

---
+++ SharedLibrary/RestrictAccessMiddleware.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace SharedLibrary;
+
+public class RestrictAccessMiddleware(RequestDelegate next)
+{
+    public async Task InvokeAsync(HttpContext context)
+    {
+        var referrer = context.Request.Headers["Referrer"].FirstOrDefault();
+
+        if (string.IsNullOrEmpty(referrer))
+        {
+            context.Response.StatusCode = StatusCodes.Status403Forbidden;
+            await context.Response.WriteAsync("You are not allowed to access this resource.");
+            return;
+        }
+        else
+        {
+            await next(context); 
+        }
+        
+        
+    }
+}
